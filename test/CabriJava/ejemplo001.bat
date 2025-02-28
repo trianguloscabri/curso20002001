00b898d3edbefca93698b1729fa3e684a8c597a9
trianguloscabri <trianguloscabri@outlook.es>
2025-03-01 00:14:14 +0100

    test Applet funcionando

---
+++ test/CabriJava/ejemplo001.bat
@@ -0,0 +1,7 @@
+ c:\jdk1.2.2\bin\appletviewer.exe  ejemplo001.htm
+
+pause
+
+
+rem   La versión de Java se ha descargado de:
+rem      - http://www.sce.carleton.ca/courses/javaResources/jdk1.2.2/download.html
